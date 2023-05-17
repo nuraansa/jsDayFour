65e9d3140a91faadb6a14d749e3da87a3d8520d0
nuraansa <nuraansa@gmail.com>
2023-05-17 10:17:19 +0200

    First Commit

---
+++ script/code.js
@@ -0,0 +1,13 @@
+let person1 ={
+    name: "Nuraan",
+    age: 23,
+};
+let person2 ={
+    name: "Sally",
+    age: 18,
+};
+console.log(person1);
+console.log(person2);
+
+let family =[person1, person2];
+console.log(family);
